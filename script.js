390ac86a2fc2026657759743aaa55de6bc4c0fb7
Jack Hoover <j.hoover1121@gmail.com>
2020-06-14 22:27:40 -0500

    initial quiz questions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variables
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
@@ -32,8 +33,8 @@ const myQuestions = [
     }
   ];
 
-  function buildQuiz(){
-    // variable to store the HTML output
+// Functions
+function buildQuiz(){ // variable to store the HTML output
     const output = [];
   
     // for each question...
@@ -67,36 +68,43 @@ const myQuestions = [
     // finally combine our output list into one string of HTML and put it on the page
     quizContainer.innerHTML = output.join('');
   }
+function showResults(){ // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+    // keep track of user's answers
+    let numCorrect = 0;
+  
+    // for each question...
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+      // if answer is correct
+      if(userAnswer === currentQuestion.correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+  
+        // color the answers green
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+  
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
 
-  myQuestions.forEach( (currentQuestion, questionNumber) => {
-    // the code we want to run for each question goes here
-    // we'll want to store the list of answer choices
-const answers = [];
 
-// and for each available answer...
-for(letter in currentQuestion.answers){
 
-  // ...add an html radio button
-  answers.push(
-    `<label>
-      <input type="radio" name="question${questionNumber}" value="${letter}">
-      ${letter} :
-      ${currentQuestion.answers[letter]}
-    </label>`
-  );
-}
-
-// add this question and its answers to the output
-output.push(
-  `<div class="question"> ${currentQuestion.question} </div>
-  <div class="answers"> ${answers.join('')} </div>`
-);
-  });
-
-function showResults(){}
-
-// display quiz right away
+// Kick things off
 buildQuiz();
 
-// on submit, show results
+// Event listeners
 submitButton.addEventListener('click', showResults);
